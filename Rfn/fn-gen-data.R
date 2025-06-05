8a9f17a966f232fa53ccb4af1f2be6cdba6f48e0
yz-apr <zhouyi0426@hotmail.com>
2025-06-05 19:38:07 +0900

    update

--- Rfn/fn-gen-data.R
+++ Rfn/fn-gen-data.R
@@ -5,19 +5,20 @@
 #' @param theta true log OR
 #' @param tau true between-study standard deviation
 #' @param rho true average control group probability
-#' @param n_min minimum value for total subjects
-#' @param n_max maximum value for total subjects
-#' @param y_min minimum value for events (ycontrol+ytreat)
-#' @param y_max maximum value for events (ycontrol+ytreat)
 #' @param Pnmax probabilities of publishing a study with maximum subjects
 #' @param Pnmin probabilities of publishing a study with minimum subjects
 #' 
-#' s=10
-#' n.med=10
-#' gr=1
-#' tau=0.5
-#' rho=0.8
-#' theta=-0.7
+# s=10
+# n.med=10
+# gr=1
+# theta=0.7
+# tau=0.05
+# rho=0.8
+# y_min=10
+# y_max=20
+# Pnmax = 0.99
+# Pnmin = 0.5
+
 gen.data1 = function(
   s, n.med, gr,
   theta,tau,rho,
@@ -44,6 +45,8 @@ gen.data1 = function(
   
   y1i = rbinom(s, size = yi, prob = study.pi) 
   y0i = yi- y1i 
+  y1i[which(y1i>n1i)]=n1i[which(y1i>n1i)]
+  y0i[which(y0i>n0i)]=n0i[which(y0i>n0i)]
   ni=n0i+n1i
   p.dt = data.frame(y1=y1i,y0=y0i,n1=n1i,n0=n0i,n=ni)
   
@@ -89,83 +92,76 @@ gen.data1 = function(
 #' @param Pnmax probabilities of publishing a study with maximum subjects
 #' @param Pnmin probabilities of publishing a study with minimum subjects
 #' 
-gen.data2 = function(
-    s,theta,tau,rho,
-    p0,n.med,gr,
-    Pnmax, Pnmin) {
-
-  ## number of subjects
-  n = round(rlnorm(s,log(n.med),1))
-  n = ifelse(n<10,10,n)
-  n0i = rbinom(s, n, 1 / (1 + gr)) # decides for each observation
-  n1i = n - n0i
-  
-  ## thetai=log(ORi) and deltai 
-  sigma=matrix(c(tau^2,rho*tau,rho*tau,1),2,2)
-  m = MASS::mvrnorm(s,c(theta,0),sigma)
-  thetai=m[,1]
-  deltai=m[,2]
-  
-  ## empirical ORi
-  ORi = exp(thetai)
-
-  ## true p0
-  mu0 = qlogis(p0) ## logit(p0) baseline
-  
-  ## empirical logit(p0i)
-  logitp0i = rnorm(s, mu0, tau^2 / sqrt(2))
-  # logitp1i = rnorm(s, mu0+theta, tau^2 / sqrt(2))
-  
-  ## empirical logit(p1i): thetai = logitp1i-logitp0i
-  logitp1i = thetai+logitp0i
-
-  p0i = plogis(logitp0i) ## logistic(logitp0i)
-  p1i = plogis(logitp1i)
-  
-  ## number of events
-  y0i = rbinom(s, n0i, p0i)
-  y1i = rbinom(s, n1i, p1i)
-
-  p.dt = data.frame(y0=y0i,y1=y1i,n0=n0i,n1=n1i,n=n)
-  
-  ## selective publication
-  n_min=min(n) 
-  n_max=max(n)
-  a1=(qnorm(Pnmax)-qnorm(Pnmin))/(sqrt(n_max)-sqrt(n_min))
-  a0=qnorm(Pnmax)-a1*sqrt(n_max)
-  
-  zi=a0+a1*sqrt(n)+deltai
-  p.dt$z=1*(zi>0)
-  
-  p.dt$pz= pnorm(a0+a1*sqrt(n))
-  p=mean(p.dt$pz)
-  
-  s.dt = p.dt[p.dt$z>0,]
-  M  = sum((1-s.dt$pz)/s.dt$pz)%>%round()  ## theoretical # of missing studies 
-  M.e = s-sum(p.dt$z) ## empirical # of missing studies 
-  # 
-  # n_min=min(s.dt$n) 
-  # n_max=max(s.dt$n)
-  # sa1=(qnorm(Pnmax)-qnorm(Pnmin))/(sqrt(n_max)-sqrt(n_min))
-  # sa0=qnorm(Pnmax)-a1*sqrt(n_max)
-
-  return(list(p.dt=p.dt,s.dt=s.dt,
-              m=M, e.m=M.e,
-              a1=a1,a0=a0,p=p))
-}
+# gen.data2 = function(
+#     s,theta,tau,rho,
+#     p0,n.med,gr,
+#     Pnmax, Pnmin) {
+# 
+#   ## number of subjects
+#   n = round(rlnorm(s,log(n.med),1))
+#   n = ifelse(n<10,10,n)
+#   n0i = rbinom(s, n, 1 / (1 + gr)) # decides for each observation
+#   n1i = n - n0i
+#   
+#   ## thetai=log(ORi) and deltai 
+#   sigma=matrix(c(tau^2,rho*tau,rho*tau,1),2,2)
+#   m = MASS::mvrnorm(s,c(theta,0),sigma)
+#   thetai=m[,1]
+#   deltai=m[,2]
+#   
+#   ## empirical ORi
+#   ORi = exp(thetai)
+# 
+#   ## true p0
+#   mu0 = qlogis(p0) ## logit(p0) baseline
+#   
+#   ## empirical logit(p0i)
+#   logitp0i = rnorm(s, mu0, tau^2 / sqrt(2))
+#   # logitp1i = rnorm(s, mu0+theta, tau^2 / sqrt(2))
+#   
+#   ## empirical logit(p1i): thetai = logitp1i-logitp0i
+#   logitp1i = thetai+logitp0i
+# 
+#   p0i = plogis(logitp0i) ## logistic(logitp0i)
+#   p1i = plogis(logitp1i)
+#   
+#   ## number of events
+#   y0i = rbinom(s, n0i, p0i)
+#   y1i = rbinom(s, n1i, p1i)
+# 
+#   p.dt = data.frame(y0=y0i,y1=y1i,n0=n0i,n1=n1i,n=n)
+#   
+#   ## selective publication
+#   n_min=min(n) 
+#   n_max=max(n)
+#   a1=(qnorm(Pnmax)-qnorm(Pnmin))/(sqrt(n_max)-sqrt(n_min))
+#   a0=qnorm(Pnmax)-a1*sqrt(n_max)
+#   
+#   zi=a0+a1*sqrt(n)+deltai
+#   p.dt$z=1*(zi>0)
+#   
+#   p.dt$pz= pnorm(a0+a1*sqrt(n))
+#   p=mean(p.dt$pz)
+#   
+#   s.dt = p.dt[p.dt$z>0,]
+#   M  = sum((1-s.dt$pz)/s.dt$pz)%>%round()  ## theoretical # of missing studies 
+#   M.e = s-sum(p.dt$z) ## empirical # of missing studies 
+#   # 
+#   # n_min=min(s.dt$n) 
+#   # n_max=max(s.dt$n)
+#   # sa1=(qnorm(Pnmax)-qnorm(Pnmin))/(sqrt(n_max)-sqrt(n_min))
+#   # sa0=qnorm(Pnmax)-a1*sqrt(n_max)
+# 
+#   return(list(p.dt=p.dt,s.dt=s.dt,
+#               m=M, e.m=M.e,
+#               a1=a1,a0=a0,p=p))
+# }
 
 
 ##
 ## From Ao
 ##
-
-gen.data3(
-    s=10, n.med=10, gr=1,
-    theta=-0.7,tau=0.01,rho=0.8,
-    p0=0.05,
-    Pnmax=0.99, Pnmin=0.5
-    )
-gen.data3 = function(
+gen.data2 = function(
     s, n.med, gr,
     theta,tau,rho,
     p0,
@@ -173,8 +169,8 @@ gen.data3 = function(
   
     n = round(rlnorm(s,log(n.med),1)) # total #subjects
     n = ifelse(n<10,10,n)
-    n0i = rbinom(s, n, 1 / (1 + gr)) # #control subjects
-    n1i = n - n0i # #treatment subjects
+    n0 = rbinom(s, n, 1 / (1 + gr)) # #control subjects
+    n1 = n - n0 # #treatment subjects
     
     ## thetai=log(ORi) and deltai 
     sigma=matrix(c(tau^2,rho*tau,rho*tau,1),2,2)
@@ -182,41 +178,34 @@ gen.data3 = function(
     thetai=m[,1]
     deltai=m[,2]
   
-    tOR.i = exp(thetai) # empirical odds ratio
-    #----------generate a b c d of OR,then calculate se----------------#
-    # Pc                   = runif(M,0.2,0.9) # Probability of event in the control group
-    Pci = plogis(rnorm(s, qlogis(p0), tau^2 / sqrt(2)))
-    m                    = tOR.i * Pci / ( 1 - Pci )
-    Pti                   = m / ( 1 + m )   # calculation of probability of event in the experimental group
-    # u                    = 2*round(rlnorm(s,5,1)/2,0) # total sample size
-    u = round(rlnorm(s,log(n.med),1))
-    nc= rbinom(s, u, 1 / (1 + gr))
-    nt= u-nc
-    # nt  = ifelse(u<20,10, 1/(1 + gr))
+    ORi = exp(thetai) # empirical odds ratio
+    pi0 = plogis(rnorm(s, qlogis(p0), tau^2 / sqrt(2))) # prob of control in study i
+    m = ORi * pi0 / ( 1 - pi0 )
+    pi1 = m / ( 1 + m )   # prob of event in study i
 
     all.dat = vapply(1:s,function(i){
-      a = sum(rbinom(nt[i],1,Pti[i]))
-      b = nt[i]
-      c = sum(rbinom(nc[i],1,Pci[i]))
-      d = nc[i]
-      c(a,b,c,d)
-      },c(y1i=0,n1i=0,y0i=0,n0i=0))%>%t()%>%data.frame()
+      yi1 = sum(rbinom(n1[i],1,pi1[i]))
+      ni1 = n1[i]
+      yi0 = sum(rbinom(n0[i],1,pi0[i]))
+      ni0 = n0[i]
+      c(yi1,ni1,yi0,ni0)
+      },c(y1=0,n1=0,y0=0,n0=0))%>%t()%>%data.frame()
     
-    ni = all.dat$n1i+all.dat$n0i
-    p.dt = data.frame(y1=all.dat$y1i,y0=all.dat$y0i,
-                      n1=all.dat$n1i,n0=all.dat$n0i,n=ni)
+    n = all.dat$n1+all.dat$n0
+    p.dt = data.frame(y1=all.dat$y1,y0=all.dat$y0,
+                      n1=all.dat$n1,n0=all.dat$n0,n=n)
     
     ## selective process
-    n_min=min(ni) 
-    n_max=max(ni)
+    n_min=min(n) 
+    n_max=max(n)
     
     a1=(qnorm(Pnmax)-qnorm(Pnmin))/(sqrt(n_max)-sqrt(n_min))
     a0=qnorm(Pnmax)-a1*sqrt(n_max)
     
-    zi=a0+a1*sqrt(ni)+deltai
+    zi=a0+a1*sqrt(n)+deltai
     p.dt$z=1*(zi>0)
     
-    pz=pnorm(a0+a1*sqrt(ni))
+    pz=pnorm(a0+a1*sqrt(n))
     p.dt$pz=pz
     p=mean(pz)
     
--- simulation/simu-compare-pop.R
+++ simulation/simu-compare-pop.R
@@ -17,24 +17,24 @@ library(metafor)
 
 
 ## true parameters set1 ----------
-s = c(10, 25, 50) ## #of population studies
+s = c(10, 50) ## #of population studies
 set = expand.grid(
-  t.theta = c(-0.7,0.7), ## true theta
-  t.tau = c(0.5, 0.15, 0.6), ## true tau  
-  # t.rho = c(-0.8,0.8),
-  n.median = c(50, 100), ## median number of total subjects,
+  t.theta = c(-0.7), ## true theta
+  t.tau = c(0.05, 0.5, 1), ## true tau  
+  t.rho = c(-0.8,0.8),
+  n.median = c(50), ## median number of total subjects,
   grp.r = c(1, 2), ##group ratio: treat:control
   pmax = 0.99,
   pmin = 0.6
 ) %>% arrange(t.theta,n.median)
-set$p0 = ifelse(set$n.median==50, 0,0.1)
-set$t.rho = ifelse(set$t.theta>0, 0.8, -0.8)
+set$p0 = ifelse(set$n.median==50, 0.05,0.1)
+# set$t.rho = ifelse(set$t.theta>0, 0.8, -0.8)
 set$ymin = ifelse(set$n.median==50, 0, 0)
-set$ymax = ifelse(set$n.median==50, 10, 20)
+set$ymax = ifelse(set$n.median==50, 5, 10)
 
-set.gr1 = set[set$grp.r==1,]
-set.gr2 = set[set$grp.r==2,]
-set.gr = set.gr1[1,]
+# set.gr1 = set[set$grp.r==1,]
+# set.gr2 = set[set$grp.r==2,]
+# set.gr = set.gr1[1,]
 
 ## ----------
 ## SIMULATION 
@@ -42,11 +42,12 @@ ncores = parallel::detectCores()-1
 cl = parallel::makeCluster(ncores, "SOCK")
 doSNOW::registerDoSNOW(cl)
 
-set.seed(2024)
-for(S in s[1]){
-for(i in 1){ 
-  DATA = foreach(r=1:1000, .combine = rbind,.packages=c("tidyr","mnormt","dplyr"),.errorhandling="remove")  %dorng%  {
-    S=100
+set.seed(2025)
+for(S in s){
+for(i in 1:nrow(set)){ 
+  DATA = foreach(r=1:1000, .combine = rbind,.packages=c("tidyr","mnormt","dplyr","metafor"))  %dorng%  {
+
+    set.gr = set[i,]
     pmax = set.gr$pmax
     pmin = set.gr$pmin
     
@@ -59,7 +60,7 @@ for(i in 1){
       Pnmax = pmax, Pnmin = pmin)
 
     
-    plist3 = gen.data3(
+    plist2 = gen.data2(
       s=S, n.med=set.gr$n.median, gr=set.gr$grp.r,
       theta=set.gr$t.theta,
       tau=set.gr$t.tau,
@@ -71,7 +72,7 @@ for(i in 1){
     
     ## population data and selective data
     pdata1 = plist1$p.dt
-    pdata2 = plist3$p.dt
+    pdata2 = plist2$p.dt
 
     ## data of lnOR
     data = lapply(list(pdata1,pdata2),
@@ -82,16 +83,16 @@ for(i in 1){
 
     ## set parset list
     parset.nn = list(
-          mu.bound = 2, 
+          mu.bound = 1, 
           tau.bound = 1,
           eps = 1e-3,
           init.vals = c(0.1,0.01)
           )
     parset.glmm = list(
-          mu.bound = 2,
+          mu.bound = 1,
           tau.bound = 1,
           eps = 1e-3,
-          integ.limit = 10, 
+          integ.limit = 5, 
           cub.tol = 1e-5,
           init.vals = c(0.1,0.01)
           )
@@ -101,27 +102,27 @@ for(i in 1){
       function(data) with(data, NN_LMM(yi, vi, 
         parset=parset.nn)))
     
-    pnn1 = c(fit.nn[[1]]$mu, fit.nn[[1]]$tau, rho=rep(NA,2), 
+    pnn1 = c(fit.nn[[1]]$mu, fit.nn[[1]]$tau, 
       cv = ifelse(is.null(fit.nn[[1]]$opt$convergence), NA, fit.nn[[1]]$opt$convergence))
-    pnn2 = c(fit.nn[[2]]$mu, fit.nn[[2]]$tau, rho=rep(NA,2), 
+    pnn2 = c(fit.nn[[2]]$mu, fit.nn[[2]]$tau, 
       cv = ifelse(is.null(fit.nn[[2]]$opt$convergence), NA, fit.nn[[2]]$opt$convergence))
 
     fit.hn = lapply(
       list(lpdata1,lpdata2), 
       function(data) with(data, HN_GLMM(y0, y1, n0, n1, 
         parset = parset.glmm)))
-    phn1 = c(fit.hn[[1]]$mu, fit.hn[[1]]$tau, rho=rep(NA,2), 
+    phn1 = c(fit.hn[[1]]$mu, fit.hn[[1]]$tau,
       cv = ifelse(is.null(fit.hn[[1]]$opt$convergence), NA, fit.hn[[1]]$opt$convergence))
-    phn2 = c(fit.hn[[2]]$mu, fit.hn[[2]]$tau, rho=rep(NA,2), 
+    phn2 = c(fit.hn[[2]]$mu, fit.hn[[2]]$tau, 
       cv = ifelse(is.null(fit.hn[[2]]$opt$convergence), NA, fit.hn[[2]]$opt$convergence))
 
     fit.bn = lapply(
       list(lpdata1,lpdata2), 
       function(data) with(data, BN_GLMM(y0, y1, n0, n1, 
         parset = parset.glmm)))
-    pbn1 = c(fit.bn[[1]]$mu, fit.bn[[1]]$tau, rho=rep(NA,2), 
+    pbn1 = c(fit.bn[[1]]$mu, fit.bn[[1]]$tau, 
       cv = ifelse(is.null(fit.bn[[1]]$opt$convergence), NA, fit.bn[[1]]$opt$convergence))
-    pbn2 = c(fit.bn[[2]]$mu, fit.bn[[2]]$tau, rho=rep(NA,2), 
+    pbn2 = c(fit.bn[[2]]$mu, fit.bn[[2]]$tau, 
       cv = ifelse(is.null(fit.bn[[2]]$opt$convergence), NA, fit.bn[[2]]$opt$convergence))
       
    
@@ -132,15 +133,15 @@ for(i in 1){
       pbn1,pbn2)
 
   }
-  save(DATA,file = paste0("res/data-sen-",i,"-S",SS,".RData"))
+  save(DATA,file = paste0("res-pop/data-set-",i,"-S",S,".RData"))
   
 }}
 
 parallel::stopCluster(cl)
 
-## summary
-load("res/data-sen-2-S15.RData")
-DATA[DATA[,8]==1,]=NA
-rr = dim(DATA)[1]/9
-group_indices = rep(1:9, each = rr)
-rowsum(DATA, group_indices, na.rm = T)/rr
+# ## summary
+# load("res/data-sen-2-S15.RData")
+# DATA[DATA[,8]==1,]=NA
+# rr = dim(DATA)[1]/9
+# group_indices = rep(1:9, each = rr)
+# rowsum(DATA, group_indices, na.rm = T)/rr
